ce2782db7fd4dfce268da20dea87b4c25e4cb97f
becomingjohndoe <karl.swatman@gmail.com>
2022-04-01 23:30:29 +0100

    add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+	return (
+		<div className="h-40 flex flex-col justify-end items-center text-slate-600 font-medium gap-4 w-full">
+			<div className="underline">60% of the time, it works every time.</div>
+			<div>
+				{" "}
+				<a href="https://github.com/becomingjohndoe">Follow Me</a>{" "}
+			</div>
+		</div>
+	);
+}
