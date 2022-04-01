ff29081c3f665074350ef1ada5c9494499f6e902
becomingjohndoe <karl.swatman@gmail.com>
2022-04-01 21:33:24 +0100

    add main Home component

---
+++ src/components/Home.jsx
@@ -0,0 +1,146 @@
+import { useState, useRef } from "react";
+import guesses from "../constants/valid-guesses";
+import LetterBox from "./LetterBox";
+import wordle from "../images/wordle.gif";
+const incorrect = [];
+export const getGuess = (guess) => {
+	const correct = [];
+
+	const wrongPlace = [];
+	for (const key in guess) {
+		if (guess[key][0] === "X") {
+			correct.push([guess[key][1], key]);
+		}
+		if (guess[key][0] === "O") {
+			wrongPlace.push([guess[key][1], key]);
+		}
+		if (guess[key][0] === "-") {
+			incorrect.push([guess[key][1], key]);
+		}
+	}
+	const nextGuesses = guesses.filter((g) => {
+		const upper = g.toUpperCase();
+		if (correct.every((c) => g[c[1]].toUpperCase() === c[0])) {
+			if (incorrect.every((i) => !upper.includes(i[0]))) {
+				if (wrongPlace.every((w) => upper.includes(w[0]) && upper[w[1]] !== w[0])) {
+					return g;
+				}
+			}
+		}
+		return false;
+	});
+	console.log(nextGuesses);
+	console.log(incorrect);
+	return nextGuesses;
+};
+
+export default function Home() {
+	const letterOne = useRef(null);
+	const letterTwo = useRef(null);
+	const letterThree = useRef(null);
+	const letterFour = useRef(null);
+	const letterFive = useRef(null);
+	const [guess, setGuess] = useState({
+		0: [],
+		1: [],
+		2: [],
+		3: [],
+		4: [],
+	});
+	const [next, setNext] = useState();
+	const [input, setInput] = useState("SOLVE");
+
+	const resetColors = () => {
+		letterOne.current.style.backgroundColor = "white";
+		letterTwo.current.style.backgroundColor = "white";
+		letterThree.current.style.backgroundColor = "white";
+		letterFour.current.style.backgroundColor = "white";
+		letterFive.current.style.backgroundColor = "white";
+	};
+
+	const addToGuess = (ref, letter, index, symbol, colour) => {
+		const newGuess = { ...guess };
+		newGuess[index] = [symbol, letter];
+		setGuess(newGuess);
+		ref.current.style.backgroundColor = colour;
+	};
+
+	return (
+		<>
+			<div className="mb-10 flex flex-col items-center justify-center">
+				<div className="absolute text-slate-800 drop-shadow-2xl shadow-white text-8xl z-10 font-semibold">
+					WORDLE SOLVER
+				</div>
+				<img className="h-40 w-full object-cover opacity-50" src={wordle} alt="" />
+			</div>
+			<div className="flex flex-col items-center font-sans">
+				<span className="text-4xl">Input Guess</span>
+				<input
+					onChange={(e) => {
+						if (e.target.value.length < 5) resetColors();
+						setInput(e.target.value.toUpperCase());
+					}}
+					className="border-2 border-slate-700 uppercase text-center h-20 text-4xl font-semibold"
+					type="text"
+					name=""
+					id=""
+					maxLength={5}
+				/>
+				<span className="text-4xl">Select Colours</span>
+				<div className="flex gap-2 mt-4">
+					<LetterBox
+						reference={letterOne}
+						index={0}
+						letter={input[0]}
+						addToGuess={addToGuess}
+					/>
+					<LetterBox
+						reference={letterTwo}
+						index={1}
+						letter={input[1]}
+						addToGuess={addToGuess}
+					/>
+					<LetterBox
+						reference={letterThree}
+						index={2}
+						letter={input[2]}
+						addToGuess={addToGuess}
+					/>
+					<LetterBox
+						reference={letterFour}
+						index={3}
+						letter={input[3]}
+						addToGuess={addToGuess}
+					/>
+					<LetterBox
+						reference={letterFive}
+						index={4}
+						letter={input[4]}
+						addToGuess={addToGuess}
+					/>
+				</div>
+				<div className="flex flex-col items-center">
+					<button
+						onClick={() => {
+							setNext(getGuess(guess));
+						}}
+						className="h-20 border-2 p-4 mt-4 bg-gradient-to-r from-slate-400 via-yellow-400 to-green-400 font-extrabold text-slate-900 text-2xl"
+					>
+						Get Next Guesses
+					</button>
+					{next && (
+						<div className="flex flex-col">
+							<div className="flex flex-col gap-2 items-center">
+								{next.map((n, i) => (
+									<span key={i} className="text-2xl">
+										{n}
+									</span>
+								))}
+							</div>
+						</div>
+					)}
+				</div>
+			</div>
+		</>
+	);
+}
