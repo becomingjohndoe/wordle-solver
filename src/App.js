c88b7d4571e8c3f04a0ee050fd46baaf73f5c08d
becomingjohndoe <karl.swatman@gmail.com>
2022-04-01 23:30:37 +0100

    bg color

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import "./App.css";
 import Home from "./components/Home";
 
 function App() {
-	return <Home />;
+	return (
+		<div className="bg-slate-200 min-h-screen">
+			<Home />;
+		</div>
+	);
 }
 
 export default App;
